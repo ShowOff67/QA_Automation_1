1b9f61e1be3757a03954e415ddfd15ffba6a8a94
ShowOff67 <rrsamin1.0@gmail.com>
2025-11-18 04:02:37 -0800

    fix some error in login

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # QA_Automation_1
 it is just a simple automation demo using selenium 
+
+I use the Chromium browser here , you need to install the requirements.txt first for using chromium. It will try to login with all the credintial and logout after login.
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 
@@ -20,28 +18,19 @@ options = Options()
 options.binary_location = chromium_path
 
 driver = webdriver.Chrome(options=options)
-wait = WebDriverWait(driver, 10)
-
 driver.maximize_window()
 driver.get("https://www.saucedemo.com/")
 sleep(1)
 
 def logout():
-    try:
-        driver.find_element(By.ID, "react-burger-menu-btn").click()
-        sleep(0.5)
-        driver.find_element(By.ID, "logout_sidebar_link").click()
-    except:
-        driver.refresh()
-        sleep(1)
-        driver.find_element(By.ID, "react-burger-menu-btn").click()
-        sleep(0.5)
-        driver.find_element(By.ID, "logout_sidebar_link").click()
+    driver.find_element(By.ID, "react-burger-menu-btn").click()
+    sleep(0.5)
+    driver.find_element(By.ID, "logout_sidebar_link").click()
+    sleep(1)
 
 for user in usernames:
-    wait.until(EC.presence_of_element_located((By.ID, "user-name"))).clear()
+    driver.find_element(By.ID, "user-name").clear()
     driver.find_element(By.ID, "password").clear()
-
     driver.find_element(By.ID, "user-name").send_keys(user)
     driver.find_element(By.ID, "password").send_keys(password)
     driver.find_element(By.ID, "login-button").click()
